ec1f90dced42b700eb312c98545cd8140e5432f5
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-24 16:13:22 +0100

    read-data-from-firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-17-firebase';
+  courses: Observable<any[]>; 
+
+  constructor(dbContext: AngularFireDatabase) {
+    this.courses = dbContext.list('courses').valueChanges();
+  }
 }
