b28643c93a8a628284fb6ab9b11327469e590199
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-26 16:45:15 +0100

    add-method-Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-17-firebase';
   courses: Observable<any[]>; 
 
-  constructor(dbContext: AngularFireDatabase) {
+  constructor(private dbContext: AngularFireDatabase, private elementRef: ElementRef) {
     this.courses = dbContext.list('courses').valueChanges();
+  }
+
+  Add(data: any) {
+    // const inputValue = data.value.trim(); // Get the trimmed value of the input
+    if (data !== '') { // Only push non-empty values
+      this.dbContext.list('courses').push(data.value);
+      this.elementRef.nativeElement.querySelector('input').value = ''; // Reset input value
+    }
   }
 }
