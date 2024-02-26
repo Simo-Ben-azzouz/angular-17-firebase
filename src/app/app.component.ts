a9b91e8844546c5ca4d769f6bafbf794b21946ae
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-26 17:47:09 +0100

    update-method-add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,14 @@ export class AppComponent {
   title = 'angular-17-firebase';
   courses: Observable<any[]>; 
 
-  constructor(private dbContext: AngularFireDatabase, private elementRef: ElementRef) {
+  constructor(private dbContext: AngularFireDatabase) {
     this.courses = dbContext.list('courses').valueChanges();
   }
 
   Add(data: any) {
-    // const inputValue = data.value.trim(); // Get the trimmed value of the input
-    if (data !== '') { // Only push non-empty values
-      this.dbContext.list('courses').push(data.value);
-      this.elementRef.nativeElement.querySelector('input').value = ''; // Reset input value
-    }
+    if (data !== '') 
+      {
+        this.dbContext.list('courses').push(data.value);
+      }
   }
 }
