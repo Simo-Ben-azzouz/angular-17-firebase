8e976e0be225b44b6d87e24bff71c87d7cfd24d9
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-02-26 18:32:45 +0100

    add-method-snapshotchanges()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,48 @@
 import { Component, ElementRef } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-17-firebase';
-  courses: Observable<any[]>; 
+  courses: Observable<any[]>;
+  clients: Observable<any[]>;
 
   constructor(private dbContext: AngularFireDatabase) {
-    this.courses = dbContext.list('courses').valueChanges();
+    this.courses = dbContext
+      .list('courses')
+      .snapshotChanges()
+      .pipe(
+        map((action) =>
+          action.map((course) => {
+            const key = course.payload.key;
+            const data = course.payload.val();
+            return data;
+          })
+        )
+      );
+
+      this.clients = dbContext
+      .list('clients')
+      .snapshotChanges()
+      .pipe(
+        map((action) =>
+          action.map((a) => {
+            const key = a.payload.key;
+            const data = a.payload.val();
+            return data;
+          })
+        )
+      );
   }
 
   Add(data: any) {
-    if (data !== '') 
-      {
-        this.dbContext.list('courses').push(data.value);
-      }
+    if (data !== '') {
+      this.dbContext.list('courses').push(data.value);
+    }
   }
 }
